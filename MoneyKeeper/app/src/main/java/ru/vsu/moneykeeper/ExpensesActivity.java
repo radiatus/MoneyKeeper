8ddb85adde2c5b2043e5471aa5bbc1440bc292ef
Ivan Nikon <IvanEvgNikonov@gmail.com>
2019-04-10 16:33:33 +0300

    [Close] Создал главный экран и все остальные пустые

---
+++ MoneyKeeper/app/src/main/java/ru/vsu/moneykeeper/ExpensesActivity.java
@@ -0,0 +1,13 @@
+package ru.vsu.moneykeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses);
+    }
+}
