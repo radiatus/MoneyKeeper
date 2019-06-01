ecd73c20d8f8bbeb589c3228e7b21a4c15156cd8
Ivan Nikon <IvanEvgNikonov@gmail.com>
2019-06-01 23:07:06 +0300

    Исправил дизайн. Удаление категорий и порогов. Фикс багов

--- MoneyKeeper/app/src/main/java/ru/vsu/moneykeeper/category/boundary/CategoryService.java
+++ MoneyKeeper/app/src/main/java/ru/vsu/moneykeeper/category/boundary/CategoryService.java
@@ -6,11 +6,15 @@ import java.util.List;
 
 import ru.vsu.moneykeeper.category.entity.Category;
 import ru.vsu.moneykeeper.dao.CategoryDAO;
+import ru.vsu.moneykeeper.expense.boundary.ExpenseService;
+import ru.vsu.moneykeeper.expense.entity.Expense;
 
 public class CategoryService {
     private CategoryDAO dao;
+    private ExpenseService expenseService;
 
     public CategoryService(Context context) {
+        expenseService = new ExpenseService(context, this);
         dao = new CategoryDAO(context);
     }
 
@@ -22,7 +26,13 @@ public class CategoryService {
         return dao.findById(id);
     }
 
-    public void delete(Long Id){
+    public void delete(Long id){
+        List<Expense> expenses = expenseService.getAll();
+        for (Expense expense : expenses) {
+            if (expense.getCategory().getId().equals(id))
+                expenseService.delete(expense.getId());
+        }
+        dao.delete(id);
     }
 
     public void add(Category category){
